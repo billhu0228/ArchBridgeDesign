e960ac40f6431e725a6b0a227efab012012ea95d
BillHu <billhu0228@icloud.com>
2021-08-08 12:55:22 +0800

    添加项目文件。

---
+++ src/Model/ArchAxis.cs
@@ -0,0 +1,124 @@
+﻿using MathNet.Numerics;
+using MathNet.Spatial.Euclidean;
+using MathNet.Spatial.Units;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class ArchAxis
+    {
+        public readonly double f,m,L;
+        public readonly double k, L1;
+        
+
+        public ArchAxis(double f,double m, double L)
+        {
+            this.f = f;
+            this.m = m;
+            this.L = L;
+            L1 = 0.5 * L;
+            k= Math.Log(m + Math.Sqrt(m * m - 1));
+        }
+
+        public double ArchLength
+        {
+            get
+            {
+                return get_length(L1) * 2.0; 
+            }
+        }
+
+        double get_kesi(double x0)
+        {
+            return x0 / L1;
+        }
+
+        double get_length(double x0)
+        {
+            double k0 = get_kesi(x0);
+            if (Math.Abs(k0)<1)
+            {
+                double v = Integrate.OnClosedInterval(ds, 0, k0);
+                return v;
+            }
+            else
+            {
+                double v1= Integrate.OnClosedInterval(ds, 0, 1.0);
+                double v2 = Math.Sqrt( Math.Pow((Math.Abs(x0) - L1),2) + Math.Pow((GetZ(x0) - GetZ(L1)),2));
+                return v1 + v2;
+            }
+            
+
+        }
+
+        /// <summary>
+        /// 任意x0时对应的拱轴交点.
+        /// </summary>
+        /// <param name="x0"></param>
+        /// <returns></returns>
+        public Point2D GetCenter(double x0)
+        {
+            return new Point2D(x0, GetZ(x0));
+        }
+
+        /// <summary>
+        /// 任意x0值下的z值，拱顶为0.
+        /// </summary>
+        /// <param name="x0"></param>
+        /// <returns></returns>
+        public double GetZ(double x0)
+        {
+            double kesi = get_kesi(x0);
+            if (Math.Abs(kesi)<=1)
+            {
+                return -f / (m - 1) * (Math.Cosh(k * kesi) - 1);
+            }
+            else
+            {
+                double zb = GetZ(Math.Sign(kesi) * L1);
+                double kk = Math.Tan(GetAngle(Math.Sign(kesi) * L1).Radians);
+                return (zb + kk * (kesi - Math.Sign(kesi)) * L1);
+            }
+       
+            
+        }
+
+        /// <summary>
+        /// 任意x0值下的切角值，拱顶为0，逆时针为正.
+        /// </summary>
+        /// <param name="x0"></param>
+        /// <returns></returns>
+        public Angle GetAngle(double x0)
+        {
+            double kesi = get_kesi(x0);
+            if (Math.Abs(kesi) <= 1)
+            {
+                double eta = 2 * f * k / (m - 1) / L;
+                Angle phi = Angle.FromRadians(-Math.Atan(eta * Math.Sinh(k * kesi)));
+                return phi;                
+            }
+            else
+            {
+                return GetAngle(Math.Sign(x0) * L1);
+            }
+
+        }
+
+
+        /// <summary>
+        /// 弧长微分算子
+        /// </summary>
+        /// <param name="ks">x0/L1值</param>
+        /// <returns></returns>
+        double ds(double ks)
+        {
+            double eta = 2 * f * k / (m - 1) / L;
+            return L1 * Math.Sqrt(1 + eta * eta * Math.Sinh(ks * k) * Math.Sinh(ks * k));
+        }
+
+    }
+}
---
+++ src/Model/CutSection.cs
@@ -0,0 +1,39 @@
+﻿using MathNet.Spatial.Euclidean;
+using MathNet.Spatial.Units;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class CutSection
+    {
+        public int ID;
+        public Point2D Center,RefPoint;
+        
+        public CutSection(int id,Point2D cc,Point2D refpt)
+        {
+            ID = id;
+            Center = cc;
+            RefPoint = refpt;
+        }
+
+        public Angle AngleFromHorizental
+        {
+            get
+            {
+                return (RefPoint - Center).SignedAngleTo(Vector2D.XAxis);
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("截面 x={0} , Ang={1}",Center.X,AngleFromHorizental.Degrees);
+        }
+
+
+
+    }
+}
