275dc233fb50aac11aa43c8f6ed0989997dd865e
BillHu <billhu0228@icloud.com>
2021-08-08 12:55:22 +0800

    添加项目文件。

---
+++ src/Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArchAxis aa = new ArchAxis(100, 1.5, 500);
+            var f=aa.ArchLength;
+        }
+    }
+}
