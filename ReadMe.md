e9c1fdc4c7e5816d574a81f79ed22a8ac7a64986
胡云天 <billhu0228@users.noreply.github.com>
2021-08-09 09:06:14 +0800

    Update ReadMe.md

--- ReadMe.md
+++ ReadMe.md
@@ -1,13 +1,28 @@
-**钢管拱建模流程**
+### **管拱建模流程**
 
-## 拱建模
+#### 拱建模
+
+ 1. 设置拱系
+ 2. 配置H0,H1等，生成拱对象;
+ 2. 为拱对象添加关键截面，添加桁片杆件尺寸；
+ 3. 利用关键截面生成节段对象；
+
+    ```C#
+    ArchAxis theArchAxis = new ArchAxis(100, 1.5, 500);
+    Arch m1 = new Arch(theArchAxis, 7, 14);
+    m1.AddSection(1, 0, Math.PI * 0.5, SectionType.InstallSection);
+
+    ```
+ 
+            
 
 
 
-## 立柱建模
+
+#### 立柱建模
 
 
-## 上部结构建模
+#### 上部结构建模
 
 
-## 拱脚建模
+#### 拱脚建模
--- src/Test/Program.cs
+++ src/Test/Program.cs
@@ -17,9 +17,12 @@ namespace Test
             /// 3. 为拱对象添加关键截面，添加桁片杆件尺寸；
             /// 4. 利用关键截面生成节段对象；
             /// 
-            ArchAxis aa = new ArchAxis(100, 1.5, 500);
-            
-            var f=aa.ArchLength;
+            ArchAxis theArchAxis = new ArchAxis(100, 1.5, 500);
+            Arch m1 = new Arch(theArchAxis, 7, 14);
+            m1.AddSection(1, 0, Math.PI * 0.5, SectionType.InstallSection);
+
+
+            var f=theArchAxis.ArchLength;
         }
     }
 }
